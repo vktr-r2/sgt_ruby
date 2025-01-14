fd320ad390ec64b3fe5c1b275b739b50378bbb92
vktr-r2 <vktr.r2@protonmail.com>
2025-01-13 23:27:20 -0500

    Setup User dummy data seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,46 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+users_data = [
+  {
+    fields: {
+      email: "user1@example.com",
+      password: "pw1234",
+      name: "John Daily",
+      admin: false
+    }
+  },
+  {
+    fields: {
+      email: "user2@example.com",
+      password: "pw1234",
+      name: "Eldrick Woods",
+      admin: true
+    }
+  },
+  {
+    fields: {
+      email: "user3@example.com",
+      password: "pw1234",
+      name: "Nelly Korda",
+      admin: false
+    }
+  },
+  {
+    fields: {
+      email: "user4@example.com",
+      password: "pw1234",
+      name: "Rory McIlroy",
+      admin: false
+    }
+  }
+]
+
+# Seed the users
+users_data.each do |user_data|
+  fields = user_data[:fields] # Access the fields using a symbol
+  
+  User.create!(
+    email: fields[:email], # Use symbols here
+    password: fields[:password],
+    name: fields[:name],
+    admin: fields[:admin]
+  )
+end
