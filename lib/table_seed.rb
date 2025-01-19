9fb7e23bb58122a892315e7955653b94878264f8
vktr-r2 <vktr.r2@protonmail.com>
2025-01-19 16:12:37 -0500

    Add exception handling to parent seed class

--- lib/table_seed.rb
+++ lib/table_seed.rb
@@ -1,9 +1,18 @@
 class TableSeed
   def self.seed
-    new.seed
+    new.seed_with_handling
   end
 
+  def seed_with_handling
+    begin
+      seed
+    rescue StandardError => e
+      puts "Error while seeding #{self.class.name}: #{e.message}"
+    end
+  end
+
+  # Child classes will override this method
   def seed
-    raise NotImplementedError, "You must implement the `seed` method"
+    raise NotImplementedError, "You must implement the `seed` method in #{self.class.name}"
   end
 end
