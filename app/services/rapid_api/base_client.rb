050da74d63362bbedec7009e8d4a3a281c2db82c
vktr-r2 <vktr.r2@protonmail.com>
2025-01-21 23:35:58 -0500

    Refactor to use methods instead of instance variables

---
+++ app/services/rapid_api/base_client.rb
@@ -0,0 +1,47 @@
+require "faraday"
+
+module RapidApi
+  class BaseClient
+    private
+
+    def make_request(url_path, params = {})
+      connection = Faraday.new(url: base_url) do |faraday|
+        faraday.headers = default_headers
+      end
+      begin
+        # Make the API request
+        response = connection.get(url_path, params)
+        handle_response(response)
+      rescue Faraday::TimeoutError => e
+        Rails.logger.error "API request timed out: #{e.message}"
+        nil
+      rescue Faraday::ConnectionFailed => e
+        Rails.logger.error "Failed to connect to API: #{e.message}"
+        nil
+      rescue Faraday::Error => e
+        Rails.logger.error "An error occurred during the API request: #{e.message}"
+        nil
+      end
+    end
+
+    def base_url
+      "https://live-golf-data.p.rapidapi.com/"
+    end
+
+    def default_headers
+      {
+        "X-RapidAPI-Key": Rails.application.credentials.config[:rapid_api][:api_key],
+        "X-RapidAPI-Host": "live-golf-data.p.rapidapi.com"
+      }
+    end
+
+    def handle_response(response)
+      if response.success?
+        JSON.parse(response.body)
+      else
+        Rails.logger.error "API request failed: #{response.status} - #{response.body}"
+        nil
+      end
+    end
+  end
+end
