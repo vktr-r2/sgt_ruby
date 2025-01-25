b59dd9ee56efa36d14d1e688eb52ada9899ceb34
vktr-r2 <vktr.r2@protonmail.com>
2025-01-25 11:54:56 -0500

    Refactor the loop through tournaments in the sched to importer

--- app/services/mappers/schedule_mapper.rb
+++ app/services/mappers/schedule_mapper.rb
@@ -5,17 +5,15 @@ module Mappers
     end
 
     def map_to_attributes
-      @sched_data[:schedule].each do |sched_tourn|
-        {
-          "tournament_id" => sched_tourn["tournId"],
-          "name" => sched_tourn["name"],
-          "start_date" => sched_tourn["date"]["start"],
-          "end_date" => sched_tourn["date"]["end"],
-          "week_number" => sched_tourn["date"]["weekNumber"],
-          "year" => Date.parse(sched_tourn["date"]["start"]).year,
-          "format" => sched_tourn["format"]
-        }
-      end
+      {
+        "tournament_id" => sched_tourn["tournId"],
+        "name" => sched_tourn["name"],
+        "start_date" => sched_tourn["date"]["start"],
+        "end_date" => sched_tourn["date"]["end"],
+        "week_number" => sched_tourn["date"]["weekNumber"],
+        "year" => Date.parse(sched_tourn["date"]["start"]).year,
+        "format" => sched_tourn["format"]
+      }
     end
   end
 end
