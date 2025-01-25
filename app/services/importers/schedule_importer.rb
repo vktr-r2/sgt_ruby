0ed1f12f7444cfeb5ec78f038dc983adc6f0db31
vktr-r2 <vktr.r2@protonmail.com>
2025-01-25 11:53:45 -0500

    Add TournamentImporter service class

---
+++ app/services/importers/schedule_importer.rb
@@ -0,0 +1,32 @@
+module Importers
+  class ScheduleImporter
+    def initialize(sched_data)
+      @sched_data = sched_data
+    end
+
+    def process
+      @sched_data["schedule"].each do |sched_tourn|
+        mapped_data = Mappers::ScheduleMapper.new(sched_tourn).map_to_attributes
+        save_schedule(mapped_data)
+      end
+    end
+
+    private
+    def save_schedule(attributes)
+      tournament = Tournament.find_or_initialize_by(tournament_id: attributes[:tournament_id], year: attributes[:year])
+      tournament.assign_attributes(attributes)
+
+      begin
+        if tournament.save
+          Rails.logger.info "Tournament #{tournament.name} for year #{tournament.year} saved successfully."
+        else
+          Rails.logger.error "Validation failed for tournament: #{tournament.errors.full_messages.join(', ')}"
+        end
+      rescue ActiveRecord::RecordNotUnique => e
+        Rails.logger.error "Unique constraint violation for tournament_id: #{attributes[:tournament_id]}, year: #{attributes[:year]}. Error: #{e.message}"
+      rescue StandardError => e
+        Rails.logger.error "An unexpected error occurred while saving tournament: #{e.message}"
+      end
+    end
+  end
+end
