3246d539647866f85a1400c677a6a662698a87e7
vktr-r2 <vktr.r2@protonmail.com>
2025-01-26 23:14:13 -0500

    Add basic verification to Tournament model

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,2 +1,13 @@
 class Tournament < ApplicationRecord
+  validates :tournament_id, presence: true
+  validates :name, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :week_number, presence: true
+  validates :year, presence: true
+  validates :format, presence: true
 end
+
+# should probably consider allowing null for all fields not mentioned above
+# Also, should there be one service that handles imports for tournaments, and it can dynamically handle both schedule and tournament inserts?
+#
