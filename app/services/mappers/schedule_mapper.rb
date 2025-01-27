87e20aa3dc44a932f67520d19275a5bc45590a79
vktr-r2 <vktr.r2@protonmail.com>
2025-01-26 23:16:41 -0500

    Add schedule_mapper to map API data to Schedule model

--- app/services/mappers/schedule_mapper.rb
+++ app/services/mappers/schedule_mapper.rb
@@ -1,18 +1,19 @@
 module Mappers
   class ScheduleMapper
+    include ApplicationHelper::DateOperations
     def initialize(sched_data)
       @sched_data = sched_data
     end
 
     def map_to_attributes
       {
-        "tournament_id" => sched_tourn["tournId"],
-        "name" => sched_tourn["name"],
-        "start_date" => sched_tourn["date"]["start"],
-        "end_date" => sched_tourn["date"]["end"],
-        "week_number" => sched_tourn["date"]["weekNumber"],
-        "year" => Date.parse(sched_tourn["date"]["start"]).year,
-        "format" => sched_tourn["format"]
+        "tournament_id" => @sched_data["tournId"],
+        "name" => @sched_data["name"],
+        "start_date" => ApplicationHelper::DateOperations.date_hash_to_time_obj(@sched_data["date"]["start"]),
+        "end_date" => ApplicationHelper::DateOperations.date_hash_to_time_obj(@sched_data["date"]["end"]),
+        "week_number" => @sched_data["date"]["weekNumber"],
+        "year" => ApplicationHelper::DateOperations.extract_year_from_date_hash(@sched_data["date"]["start"]),
+        "format" => @sched_data["format"]
       }
     end
   end
