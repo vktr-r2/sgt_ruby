336bdf7201a95f82bd354a52e81a29d982ddf07f
vktr-r2 <vktr.r2@protonmail.com>
2025-01-26 23:15:23 -0500

    Add helper functions to format weird date hashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  module DateOperations
+    def self.date_hash_to_time_obj(hash)
+      # Extract the timestamp and convert it to seconds
+      timestamp_ms = hash.dig("$date", "$numberLong").to_i
+      timestamp_seconds = timestamp_ms / 1000
+
+      # Convert to Time object
+      datetime = Time.at(timestamp_seconds)
+      datetime
+    end
+
+    def self.extract_year_from_date_hash(hash)
+      year = self.date_hash_to_time_obj(hash).year
+      year
+    end
+  end
 end
