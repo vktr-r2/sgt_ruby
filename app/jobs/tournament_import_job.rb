c66ce0ac757e959a746f5c9e7b33ab138b40704b
vktr-r2 <vktr.r2@protonmail.com>
2025-01-27 23:43:08 -0500

    Add job to process tournament import -> save flow

---
+++ app/jobs/tournament_import_job.rb
@@ -0,0 +1,10 @@
+class TournamentImportJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    api_data = RapidApi::TournamentClient.new.fetch(ApplicationHelper::DataEvaluations.determine_current_tourn_id)
+    nil if api_data.blank?
+
+  Importers::TournamentImporter.new(api_data).process
+  end
+end
