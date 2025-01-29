361d5ae255aca30d1ccd4f30f668fa7bdca1229b
vktr-r2 <vktr.r2@protonmail.com>
2025-01-28 20:05:54 -0500

    Add tournament_id, year to attributes so Tournament can be found in db

--- app/services/mappers/tournament_mapper.rb
+++ app/services/mappers/tournament_mapper.rb
@@ -1,11 +1,17 @@
 module Mappers
   class TournamentMapper
+    def initialize(tourn_data)
+      @tourn_data = tourn_data
+    end
+
     def map_to_attributes
       {
+        "tournament_id" => @tourn_data["tournId"],
+        "year" => ApplicationHelper::DateOperations.extract_year_from_date_hash(@tourn_data["date"]["start"]),
         "source_id" => @tourn_data.dig("_id", "$oid"),
         "golf_course" => @tourn_data["courses"][0]["courseName"],
         "location" => @tourn_data["courses"][0]["location"].to_json,
-        "timezone" => @tourn_data["timeZone"],
+        "time_zone" => @tourn_data["timeZone"],
         "format" => @tourn_data["format"],
         "major_championship" => ApplicationHelper::DataEvaluations.is_major?(@tourn_data["name"]),
         "par" => @tourn_data["courses"][0]["parTotal"]
