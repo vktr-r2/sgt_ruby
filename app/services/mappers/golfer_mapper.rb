0ccbbc001e4a4b3437e168e9231901fceba968ba
vktr-r2 <vktr.r2@protonmail.com>
2025-01-30 23:38:14 -0500

    Add GolferMapper

---
+++ app/services/mappers/golfer_mapper.rb
@@ -0,0 +1,17 @@
+module Mappers
+  class GolferMapper
+    def initialize(golfer_data, tournament_unique_id)
+      @golfer_data = golfer_data
+      @tournament_unique_id = tournament_unique_id
+    end
+
+    def map_to_attributes
+      {
+        "source_id" => @golfer_data["playerId"],
+        "f_name" => @golfer_data["firstName"],
+        "l_name" => @golfer_data["lastName"],
+        "last_active_tourney" => @tournament_unique_id # References tounament.unique_id
+      }
+    end
+  end
+end
