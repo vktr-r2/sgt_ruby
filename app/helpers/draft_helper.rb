fdf0d0e387be19f9a2a65ff82e17a8a3655987a3
vktr-r2 <vktr.r2@protonmail.com>
2025-02-04 00:41:30 -0500

    Add helpers to fetch golfers list for current tourn

--- app/helpers/draft_helper.rb
+++ app/helpers/draft_helper.rb
@@ -1,6 +1,14 @@
 module DraftHelper
-  module PlayersData
-    def self.get_current_tourn_players
+  module GolferData
+    def self.get_current_tourn_golfers
+      unique_id = ApplicationHelper::TournamentEvaluations.determine_current_tourn_unique_id
+      golfers = Golfer.where(last_active_tourney: unique_id)
+      golfers
+    end
+
+    def self.sort_current_tourn_golfers(golfers)
+      sorted_golfers = golfers.sort_by { |golfer| golfer[:l_name] }
+      sorted_golfers
     end
   end
 end
