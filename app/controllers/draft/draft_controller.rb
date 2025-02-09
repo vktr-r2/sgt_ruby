e35905f3fc3c57bf8c96e7f9294e349ef349a46f
vktr-r2 <vktr.r2@protonmail.com>
2025-02-08 23:32:37 -0500

    Set user and pass to view

--- app/controllers/draft/draft_controller.rb
+++ app/controllers/draft/draft_controller.rb
@@ -1,12 +1,14 @@
 module Draft
   class DraftController < ApplicationController
+    before_action :authenticate_user!
     def pick
       @golfers = DraftHelper::GolferData.get_current_tourn_golfers
-      @sorted_golfers = DraftHelper::GolferData.sort_current_tourn_golfers(@golfers)
-      @sorted_golfers
+      @user = current_user
+      return @golfers, @user
     end
 
     def submit
+      @user = current_user
     end
 
     def review
