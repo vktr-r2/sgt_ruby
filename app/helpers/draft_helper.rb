a25ad82aad308f954da0d089b695774c959fb807
vktr-r2 <vktr.r2@protonmail.com>
2025-02-08 22:36:01 -0500

    Refactor get_current_tourn_golfers to use sort_golfers method

--- app/helpers/draft_helper.rb
+++ app/helpers/draft_helper.rb
@@ -3,10 +3,10 @@ module DraftHelper
     def self.get_current_tourn_golfers
       unique_id = ApplicationHelper::TournamentEvaluations.determine_current_tourn_unique_id
       golfers = Golfer.where(last_active_tourney: unique_id)
-      golfers
+      sort_golfers(golfers)
     end
 
-    def self.sort_current_tourn_golfers(golfers)
+    def self.sort_golfers(golfers)
       sorted_golfers = golfers.sort_by { |golfer| golfer[:l_name] }
       sorted_golfers
     end
