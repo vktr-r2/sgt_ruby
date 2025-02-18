420a2f184cfe49fa8d240fa28561a94090380675
vktr-r2 <vktr.r2@protonmail.com>
2025-02-17 21:40:53 -0500

    Fix bug - change intialize method to setup method in job classes

--- app/jobs/schedule_import_job.rb
+++ app/jobs/schedule_import_job.rb
@@ -1,12 +1,13 @@
 class ScheduleImportJob < ApplicationJob
   queue_as :default
 
-  def initialize
-    @api_data = RapidApi::ScheduleClient.new.fetch
-  end
-
   def perform
+    setup
     return nil if api_data.blank?
     Importers::ScheduleImporter.new(api_data).process
+  end
+
+  def setup
+    @api_data = RapidApi::ScheduleClient.new.fetch
   end
 end
--- app/jobs/tournament_import_job.rb
+++ app/jobs/tournament_import_job.rb
@@ -1,14 +1,15 @@
 class TournamentImportJob < ApplicationJob
   queue_as :default
 
-  def initialize
-    @tournament_service = BusinessLogic::TournamentService.new
-  end
-
   def perform
-    api_data = RapidApi::TournamentClient.new.fetch(tournament_service.current_tournament_id)
+    setup
+    api_data = RapidApi::TournamentClient.new.fetch(@tournament_service.current_tournament_id)
     return nil if api_data.blank?
 
     Importers::TournamentImporter.new(api_data).process
+  end
+
+  def setup
+    @tournament_service = BusinessLogic::TournamentService.new
   end
 end
