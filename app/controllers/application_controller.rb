214fbaf998d8e1dd8e81bb8e325512b00785be2e
vktr-r2 <vktr.r2@protonmail.com>
2025-08-25 14:39:49 -0400

    Convert to ActionController::API with token auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  include ActionController::Cookies
+  include Devise::Controllers::Helpers
+  
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  allow_browser versions: :modern
+  
+  private
+  
+  def authenticate_user!
+    token = request.headers['Authorization']&.split(' ')&.last
+    return render json: { error: 'Unauthorized' }, status: :unauthorized unless token
+    
+    @current_user = User.find_by(authentication_token: token)
+    render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user
+  end
+  
+  def current_user
+    @current_user
+  end
 
   protected
 
