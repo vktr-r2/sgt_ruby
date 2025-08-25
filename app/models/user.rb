a032a77145bc9cb27943d590fbb979be63e68ad9
vktr-r2 <vktr.r2@protonmail.com>
2025-08-25 14:42:22 -0400

    Add authentication token support to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   ADMIN_EMAILS = [ "vik.ristic@gmail.com" ]
 
   before_create :set_admin_status
+  before_save :ensure_authentication_token
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -9,8 +10,25 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  
+  def ensure_authentication_token!
+    self.authentication_token = generate_authentication_token if authentication_token.blank?
+    save!
+  end
 
   private
+  
+  def ensure_authentication_token
+    self.authentication_token = generate_authentication_token if authentication_token.blank?
+  end
+  
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
+  end
+  
   def set_admin_status
     self.admin = ADMIN_EMAILS.include?(email)
   end
