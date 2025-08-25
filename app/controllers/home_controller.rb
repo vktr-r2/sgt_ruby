dacdb5270b6cb4ad447fd7a5fa9510301e5b433b
vktr-r2 <vktr.r2@protonmail.com>
2025-08-25 14:40:19 -0400

    Return JSON with tourn data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,22 @@
 class HomeController < ApplicationController
   def index
-    render
+    tournament_service = BusinessLogic::TournamentService.new
+    current_tournament = tournament_service.current_tournament
+    
+    render json: {
+      current_tournament: current_tournament ? {
+        id: current_tournament.id,
+        name: current_tournament.name,
+        start_date: current_tournament.start_date,
+        end_date: current_tournament.end_date,
+        week_number: current_tournament.week_number,
+        year: current_tournament.year,
+        format: current_tournament.format
+      } : nil,
+      app_info: {
+        name: "Spreadsheet Golf Tour",
+        version: "1.0.0"
+      }
+    }
   end
 end
