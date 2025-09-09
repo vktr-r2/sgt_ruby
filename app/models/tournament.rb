ca2ab6b48dc30c2f8ed026041b18018487bedc1d
vktr-r2 <vktr.r2@protonmail.com>
2025-09-08 22:18:29 -0400

    Add draft window related methods to model

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -8,4 +8,18 @@ class Tournament < ApplicationRecord
   validates :format, presence: true
   has_many :players, foreign_key: "last_active_tourney", primary_key: "unique_id"
   has_many :match_picks, foreign_key: "tournament_id", primary_key: "id", dependent: :destroy
+
+  def draft_window_start
+    # Two days before tournament starts at 00:00:00
+    (start_date - 2.days).beginning_of_day
+  end
+
+  def draft_window_end  
+    # One day before tournament starts at 23:59:59
+    (start_date - 1.day).end_of_day
+  end
+
+  def draft_window_open?(current_time = Time.zone.now)
+    current_time >= draft_window_start && current_time <= draft_window_end
+  end
 end
