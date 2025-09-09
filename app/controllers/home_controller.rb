a8816bd409d39c711427b17137b8502cc3b5312d
vktr-r2 <vktr.r2@protonmail.com>
2025-09-08 22:20:57 -0400

    Add draft window service to home controller for front end API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     tournament_service = BusinessLogic::TournamentService.new
     current_tournament = tournament_service.current_tournament
+    draft_window_service = BusinessLogic::DraftWindowService.new(current_tournament)
     
     render json: {
       current_tournament: current_tournament ? {
@@ -11,7 +12,13 @@ class HomeController < ApplicationController
         end_date: current_tournament.end_date,
         week_number: current_tournament.week_number,
         year: current_tournament.year,
-        format: current_tournament.format
+        format: current_tournament.format,
+        draft_window: {
+          start: current_tournament.draft_window_start,
+          end: current_tournament.draft_window_end,
+          status: draft_window_service.draft_window_status,
+          is_open: draft_window_service.draft_open?
+        }
       } : nil,
       app_info: {
         name: "Spreadsheet Golf Tour",
