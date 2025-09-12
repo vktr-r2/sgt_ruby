e8b4974c0456a7f94c81ee965dbbc929f9990da3
vktr-r2 <vktr.r2@protonmail.com>
2025-09-11 20:39:11 -0400

    Move GOLFER_SELECTION_LIMIT constant to model

--- app/models/match_pick.rb
+++ app/models/match_pick.rb
@@ -2,4 +2,7 @@ class MatchPick < ApplicationRecord
   belongs_to :user
   belongs_to :tournament
   belongs_to :golfer
+
+  # Golfer selection limit per season (Scottie Scheffler rule)
+  GOLFER_SELECTION_LIMIT = 3
 end
