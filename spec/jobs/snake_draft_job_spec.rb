2f8c5dbf7a1a78e7c799061117dc735a29e0cd19
vktr-r2 <vktr.r2@protonmail.com>
2025-12-05 22:59:57 -0500

    Simplify SnakeDraftJob test

--- spec/jobs/snake_draft_job_spec.rb
+++ spec/jobs/snake_draft_job_spec.rb
@@ -9,15 +9,15 @@ RSpec.describe SnakeDraftJob, type: :job do
 
   describe "#perform" do
     context "when draft is successful" do
-      let(:tournament) { instance_double(Tournament, name: "Test Tournament") }
+      let(:tournament) { double("Tournament", name: "Test Tournament") }
       let(:success_result) { { success: true, tournament: tournament, draft_order: [], assigned_picks: 32 } }
 
       before do
         allow(service).to receive(:execute_draft).and_return(success_result)
       end
 
-      it "logs success message" do
-        expect(Rails.logger).to receive(:info).with("Snake draft completed for tournament: Test Tournament")
+      it "calls execute_draft on the service" do
+        expect(service).to receive(:execute_draft).and_return(success_result)
         described_class.perform_now
       end
     end
