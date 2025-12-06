9154963c8d8cda111e4a6c0355d65d38947342ff
vktr-r2 <vktr.r2@protonmail.com>
2025-12-05 22:47:53 -0500

    Add success test for SnakeDraftJob

--- spec/jobs/snake_draft_job_spec.rb
+++ spec/jobs/snake_draft_job_spec.rb
@@ -8,6 +8,18 @@ RSpec.describe SnakeDraftJob, type: :job do
   end
 
   describe "#perform" do
-    # Tests will be added here
+    context "when draft is successful" do
+      let(:tournament) { instance_double(Tournament, name: "Test Tournament") }
+      let(:success_result) { { success: true, tournament: tournament, draft_order: [], assigned_picks: 32 } }
+
+      before do
+        allow(service).to receive(:execute_draft).and_return(success_result)
+      end
+
+      it "logs success message" do
+        expect(Rails.logger).to receive(:info).with("Snake draft completed for tournament: Test Tournament")
+        described_class.perform_now
+      end
+    end
   end
 end
