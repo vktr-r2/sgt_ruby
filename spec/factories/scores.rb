340ce38030949e497d85b98f9f8df127cba601ac
vktr-r2 <vktr.r2@protonmail.com>
2025-12-05 22:23:54 -0500

    Add factories for match results and scores

---
+++ spec/factories/scores.rb
@@ -0,0 +1,35 @@
+FactoryBot.define do
+  factory :score do
+    association :match_pick
+    score { rand(65..75) }
+    round { 1 }
+
+    trait :excellent do
+      score { rand(63..67) }
+    end
+
+    trait :average do
+      score { rand(68..72) }
+    end
+
+    trait :poor do
+      score { rand(73..78) }
+    end
+
+    trait :round_1 do
+      round { 1 }
+    end
+
+    trait :round_2 do
+      round { 2 }
+    end
+
+    trait :round_3 do
+      round { 3 }
+    end
+
+    trait :round_4 do
+      round { 4 }
+    end
+  end
+end
