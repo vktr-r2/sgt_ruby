47bf1213d35d2c7f057f9b7c4417ba5725404e2a
vktr-r2 <vktr.r2@protonmail.com>
2025-12-22 22:42:52 -0500

    Add test for LeaderboardImportJob success

---
+++ spec/jobs/leaderboard_import_job_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe LeaderboardImportJob, type: :job do
+  let(:tournament) { create(:tournament, tournament_id: "464", name: "Test Tournament") }
+  let(:api_data) { { "leaderboardRows" => [] } }
+  let(:leaderboard_client) { instance_double(RapidApi::LeaderboardClient, fetch: api_data) }
+  let(:leaderboard_importer) { instance_double(Importers::LeaderboardImporter, process: true) }
+
+  before do
+    tournament_service_double = instance_double(BusinessLogic::TournamentService)
+    allow(BusinessLogic::TournamentService).to receive(:new).and_return(tournament_service_double)
+    allow(tournament_service_double).to receive(:current_tournament).and_return(tournament)
+
+    allow(RapidApi::LeaderboardClient).to receive(:new).and_return(leaderboard_client)
+    allow(leaderboard_client).to receive(:fetch).with(tournament.tournament_id).and_return(api_data)
+    allow(Importers::LeaderboardImporter).to receive(:new).with(api_data, tournament).and_return(leaderboard_importer)
+  end
+
+  it "fetches API data and processes leaderboard import" do
+    described_class.perform_now
+
+    expect(BusinessLogic::TournamentService).to have_received(:new)
+    expect(leaderboard_client).to have_received(:fetch).with(tournament.tournament_id)
+    expect(Importers::LeaderboardImporter).to have_received(:new).with(api_data, tournament)
+    expect(leaderboard_importer).to have_received(:process)
+  end
+end
