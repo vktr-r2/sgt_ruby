97ebe0846dc5b06d8a55b53bec1cc99af6f66bba
vktr-r2 <vktr.r2@protonmail.com>
2026-02-09 11:47:52 -0500

    Add error handling to password reset

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -16,7 +16,13 @@ class Users::PasswordsController < ApplicationController
     user = User.find_by(email: email)
 
     if user
-      user.send_reset_password_instructions
+      begin
+        user.send_reset_password_instructions
+      rescue StandardError => e
+        Rails.logger.error "Password reset email failed: #{e.class} - #{e.message}"
+        # In production, still return success for security, but log the error
+        return render json: { message: "Password reset instructions sent" }
+      end
     end
 
     # Always return success for security (don't reveal if email exists)
